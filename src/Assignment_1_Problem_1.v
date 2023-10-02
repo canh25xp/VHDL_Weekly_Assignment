81dd887bd9db3aa8b39eebb327af162b3188b8e5
Cảnh Ngô <canh25xp@gmail.com>
2023-10-02 07:08:00 +0700

    Add Assignment_1_Problem_2.v

--- src/Assignment_1_Problem_1.v
+++ src/Assignment_1_Problem_1.v
@@ -1,4 +1,4 @@
-module Problem1(A,B,C,D,F);
+module Problem1(F,A,B,C,D);
 input A,B,C,D;
 output F;
 wire S1,S2,S3,S4;
@@ -8,25 +8,3 @@ and A1(S3,S1,C);
 and A2(S4,S1,S2);
 or O1(F,A,S3,S4);
 endmodule
-
-// module Problem2_tb();
-// reg a,b,c,d;
-// wire out;
-// Problem2 problem2(a,b,c,d,out);
-
-// initial begin
-//     #10 a = 
-//     #10
-//     #10
-//     #10
-//     #10
-//     #10
-//     #10
-//     #10
-//     #10
-//     #10
-//     #10
-//     #10
-// end
-
-// endmodule
---
+++ src/Assignment_1_Problem_2.v
@@ -0,0 +1,13 @@
+module mux4_to_1 (F, i0, i1, i2, i3, s1, s0);
+output F;
+input i0, i1, i2, i3, s1, s0;
+wire s1n, s0n;
+wire y0, y1, y2, y3;
+not (s1n, s1);
+not (s0n, s0);
+and (y0, i0, s1n, s0n);
+and (y1, i1, s1n, s0);
+and (y2, i2, s1, s0n);
+and (y3, i3, s1, s0);
+or (F, y0, y1, y2, y3);
+endmodule
